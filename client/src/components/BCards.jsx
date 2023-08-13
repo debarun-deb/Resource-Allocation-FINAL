42d05d1ae218b3e7a9c02a951f8217f2dccb7620
Samxpi <Samprit62@gmail.com>
2023-08-13 19:09:06 +0530

    commit

--- client/src/components/BCards.jsx
+++ client/src/components/BCards.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Bcards = (props) => {
   return (
     <div className='py-3 cursor-pointer flex px-3'>
-      <div className='w-[400px] h-[200px] shadow-lg shadow-gray-600 rounded overflow-hidden'>
+      <div className='w-[400px] h-[200px] shadow-lg shadow-gray-600 rounded-xl overflow-hidden'>
         <h1 className='text-md font-semibold p-2'>Event Name: {props.books.eventName}</h1>
         <h1 className='text-md font-semibold p-2'>From Date: {props.books.startDate}</h1>
         <h1 className='text-md font-semibold p-2'>To Date:</h1>
--- client/src/screens/Bookings.jsx
+++ client/src/screens/Bookings.jsx
@@ -27,7 +27,7 @@ const Bookings = () => {
   }, []);
   return (
     <div>
-      <h1 className="flex px-5 py-2 text-xl font-bold">Bookings</h1>
+      <h1 className="flex px-5 py-2 text-3xl font-bold">Bookings</h1>
       <div className="bg-[#1F6E8C] flex my-6 py-3 rounded-full">
         <h1 className="px-3 text-xl font-bold">Pending</h1>
       </div>
