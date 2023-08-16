8e25b06e5d0808d28ddfeb17c86e2c762f8e2bd2
Jatin Sharma <jaters1200@gmail.com>
2023-08-16 23:54:59 +0530

    Filtered approver requests to remove expired req

--- server/controller/ApproveController.js
+++ server/controller/ApproveController.js
@@ -1,18 +1,17 @@
 const form = require("./../models/formModel");
 const sendEmail = require("../utilities/email_sender");
-
 const pReq = 'jaters1200@gmail.com';
 
 exports.approvalRequestList = async (req, res) => {
     try {
       const currentDate = new Date();
-      const allForms = await form.find()
+      console.log(currentDate);
+      const allForms = await form.find({startDate:{$gte:currentDate}})
         .sort({
           startDate: 1, // Sort in ascending order based on startDate
         })
         .lean() // Convert Mongoose documents to plain objects
         .exec();
-  
       allForms.sort((a, b) => Math.abs(a.startDate - currentDate) - Math.abs(b.startDate - currentDate));
       const pMails = allForms.filter(item => item.email === pReq);
       const npMails = allForms.filter(item => item.email !== pReq);
--- server/router/approverRoutes.js
+++ server/router/approverRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const approver = require('../controller/ApproveController')
 
-router.route('/forApproval').get(approver.getAllSubmittedForms)
+router.route('/forApproval').get(approver.approvalRequestList);
 
 module.exports = router
 
