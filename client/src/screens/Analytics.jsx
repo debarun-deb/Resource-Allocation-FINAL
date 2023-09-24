d02c2f5210098131dee4ed75186899336b2e6372
Samxpi <samprit_202100020@smit.smu.edu.in>
2023-09-24 20:12:32 +0530

    graph

--- client/src/screens/Analytics.jsx
+++ client/src/screens/Analytics.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
 const Analytics = () => {
-  return <div>
-    <h1>Analytics</h1>
+  return (
     <div>
-        
+      <h1>Select the resource:</h1>
+      <select name="" id="">
+        <option value="Multipurpose Hall">Multipurpose Hall</option>
+        <option value="Seminar Hall">Seminar Hall</option>
+        <option value="Central Computing Facility">Central Computing Facility</option>
+      </select>
+      <div></div>
     </div>
-  </div>;
+  );
 };
 
 export default Analytics;
