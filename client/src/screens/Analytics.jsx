34633c87e238bb958bd0150067a0e1519b244132
Samxpi <samprit_202100020@smit.smu.edu.in>
2023-09-24 20:24:32 +0530

    commit

--- client/src/screens/Analytics.jsx
+++ client/src/screens/Analytics.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Analytics = () => {
+  const [resource, setResource] = useState("");
+
   return (
     <div>
       <h1>Select the resource:</h1>
-      <select name="" id="">
+      <select
+        name=""
+        id=""
+        onChange={(e) => {
+          setResource(e.target.value);
+        }}
+      >
         <option value="Multipurpose Hall">Multipurpose Hall</option>
         <option value="Seminar Hall">Seminar Hall</option>
-        <option value="Central Computing Facility">Central Computing Facility</option>
+        <option value="Central Computing Facility">
+          Central Computing Facility
+        </option>
       </select>
       <div></div>
     </div>
